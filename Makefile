911faa94ca861a2ca91dc0eabd201ce810fc05f8
nnashiki <n.nashiki.work@gmail.com>
2020-08-12 20:27:50 +0900

    add: 一旦コミット

---
+++ Makefile
@@ -0,0 +1,29 @@
+GCP_PROJECT_ID=<gcp_project_id>
+PROJECT=${GCP_PROJECT_ID}
+AIRFLOW_IMAGE_TAG=0.1
+GCR_HOST=asia.gcr.io
+GCP_ZONE=us-west1-b
+DEFAULT_SERVICE_ACCOUNT=<default_service_account>
+MACHINE_TYPE=n1-standard-2
+
+
+
+
+build:
+	docker build --rm \
+	 --build-arg AIRFLOW_DEPS="datadog,dask" \
+	 -t ariflow_failure_callback .
+
+python_ver:
+	docker run --rm -it ariflow_failure_callback python -V
+
+run:
+	docker run --rm -d -p 8080:8080 -v ${PWD}/src/dags:/usr/local/airflow/dags ariflow_failure_callback
+
+down:
+	docker-compose -p ${PROJECT} down
+
+up:
+	docker-compose -p ${PROJECT} up -d
+
+
---
+++ src/script/entrypoint.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# User-provided configuration must always be respected.
+#
+# Therefore, this script must only derives Airflow AIRFLOW__ variables from other variables
+# when the user did not provide their own configuration.
+
+TRY_LOOP="20"
+
+# Global defaults and back-compat
+: "${AIRFLOW_HOME:="/usr/local/airflow"}"
+: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
+: "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
+
+# Load DAGs examples (default: Yes)
+if [[ -z "$AIRFLOW__CORE__LOAD_EXAMPLES" && "${LOAD_EX:=n}" == n ]]; then
+  AIRFLOW__CORE__LOAD_EXAMPLES=False
+fi
+
+export \
+  AIRFLOW_HOME \
+  AIRFLOW__CORE__EXECUTOR \
+  AIRFLOW__CORE__FERNET_KEY \
+  AIRFLOW__CORE__LOAD_EXAMPLES \
+
+
+wait_for_port() {
+  local name="$1" host="$2" port="$3"
+  local j=0
+  while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
+    j=$((j+1))
+    if [ $j -ge $TRY_LOOP ]; then
+      echo >&2 "$(date) - $host:$port still not reachable, giving up"
+      exit 1
+    fi
+    echo "$(date) - waiting for $name... $j/$TRY_LOOP"
+    sleep 5
+  done
+}
+
+# Other executors than SequentialExecutor drive the need for an SQL database, here PostgreSQL is used
+if [ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]; then
+  # Check if the user has provided explicit Airflow configuration concerning the database
+  if [ -z "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" ]; then
+    # Default values corresponding to the default compose files
+    : "${POSTGRES_HOST:="postgres"}"
+    : "${POSTGRES_PORT:="5432"}"
+    : "${POSTGRES_USER:="airflow"}"
+    : "${POSTGRES_PASSWORD:="airflow"}"
+    : "${POSTGRES_DB:="airflow"}"
+    : "${POSTGRES_EXTRAS:-""}"
+
+    AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
+    export AIRFLOW__CORE__SQL_ALCHEMY_CONN
+
+    # Check if the user has provided explicit Airflow configuration for the broker's connection to the database
+    if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
+      AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
+      export AIRFLOW__CELERY__RESULT_BACKEND
+    fi
+  else
+    if [[ "$AIRFLOW__CORE__EXECUTOR" == "CeleryExecutor" && -z "$AIRFLOW__CELERY__RESULT_BACKEND" ]]; then
+      >&2 printf '%s\n' "FATAL: if you set AIRFLOW__CORE__SQL_ALCHEMY_CONN manually with CeleryExecutor you must also set AIRFLOW__CELERY__RESULT_BACKEND"
+      exit 1
+    fi
+
+    # Derive useful variables from the AIRFLOW__ variables provided explicitly by the user
+    POSTGRES_ENDPOINT=$(echo -n "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" | cut -d '/' -f3 | sed -e 's,.*@,,')
+    POSTGRES_HOST=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f1)
+    POSTGRES_PORT=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f2)
+  fi
+
+  wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
+fi
+
+airflow initdb;
+airflow scheduler &
+airflow webserver
+
